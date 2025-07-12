d5c071e10e4c65e847346bb92576164ccb4a6a99
[ghyslain12] <[ghyslain12@gmail.com]>
2025-07-12 16:49:33 +0200

    refacto

--- project/tests/Feature/Api/Controller/MaterialControllerTest.php
+++ project/tests/Feature/Api/Controller/MaterialControllerTest.php
@@ -65,7 +65,7 @@ class MaterialControllerTest extends TestCase
                      '*' => ['id', 'designation', 'created_at', 'updated_at'],
                  ]);
     }
-/*
+
     #[Test]
     public function it_fails_to_list_materials_without_auth()
     {
@@ -187,5 +187,5 @@ class MaterialControllerTest extends TestCase
         $response = $this->deleteJson('/api/material/999');
 
         $response->assertStatus(404);
-    }*/
+    }
 }
